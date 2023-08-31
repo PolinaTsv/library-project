f651ab0c10c3027ddc3d32b8d4616ea14dc8c1bc
PolinaTsv <polina.tsvetkova7@gmail.com>
2023-08-31 22:53:09 +0700

    Learning

---
+++ routes/book.routes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+
+const Book = require("../models/Book.model.js");
+
+router.get("/books/create", (req, res) => res.render("books/book-create.hbs"));
+
+// POST route to save a new book to the database in the books collection
+router.post("/books/create", (req, res, next) => {
+  // console.log(req.body);
+  const { title, author, description, rating } = req.body;
+
+  Book.create({ title, author, description, rating })
+    // .then(bookFromDB => console.log(`New book created: ${bookFromDB.title}.`))
+    .then(() => res.redirect("/books"))
+    .catch((error) => next(error));
+});
+// GET route to retrieve and display all the books
+router.get("/books", (req, res, next) => {
+  Book.find()
+    .then((allTheBooksFromDB) => {
+      // -> allTheBooksFromDB is a placeholder, it can be any word
+      console.log("Retrieved books from DB:", allTheBooksFromDB);
+
+      // we call the render method after we obtain the books data from the database -> allTheBooksFromDB
+      res.render("books/books-list.hbs", { books: allTheBooksFromDB }); // pass `allTheBooksFromDB` to the view (as a variable books to be used in the HBS)
+    })
+    .catch((error) => {
+      console.log("Error while getting the books from the DB: ", error);
+
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
+});
+// GET route to retrieve and display details of a specific book
+
+router.get("/books/:bookId", (req, res, next) => {
+  const { bookId } = req.params;
+
+  Book.findById(bookId)
+    .then((theBook) => res.render("books/book-details.hbs", { book: theBook }))
+    .catch((error) => {
+      console.log("Error while retrieving book details: ", error);
+
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
+});
+
+module.exports = router;
